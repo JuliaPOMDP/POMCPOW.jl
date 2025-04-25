5372bf0aab4269c116b842efddbe6e3edcdafd23
Zachary Sunberg <zachary.sunberg@colorado.edu>
2025-04-25 15:57:37 -0600

    fixed updater

--- src/updater.jl
+++ src/updater.jl
@@ -1,4 +1,4 @@
 function POMDPs.updater(p::POMCPOWPlanner)
     rng = MersenneTwister(rand(p.solver.rng, UInt32))
-    return BootstrapFilter(p.problem, 10*p.solver.tree_queries, rng)
+    return BootstrapFilter(p.problem, 10*p.solver.tree_queries; rng)
 end
